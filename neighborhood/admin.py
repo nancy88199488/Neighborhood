ba9f2d1aaabfaa5551f60562de027bfaa84aa648
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-23 23:35:41 +0300

    added and updated the admin file

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Business,Neighborhood,Profile,
 
 # Register your models here.
+
+
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
