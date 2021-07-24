95a4f431f0462a998b4ea3b0cc929812b1c2915a
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-24 23:53:27 +0300

    modified the urls file

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,20 +1,17 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', views.index, name= 'index'),
-    url(r'^search/', views.search_businesses, name='search_results'),
-    url(r'^business/(\d+)', views.get_business, name='business_results'),
-    url(r'^new/business$', views.new_business, name='new-business'),
-    url(r'^new/post$', views.new_post, name='new-post'),
-    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
-   
-
-    
-
+    path('', views.index, name= 'index'),
+    path('search/', views.search_businesses, name='search_results'),
+    path('business/(\d+)', views.get_business, name='business_results'),
+    path('new/business', views.new_business, name='new-business'),
+    path('new/post', views.new_post, name='new-post'),
+    path('accounts/profile/', views.user_profiles, name='profile'),
+     
 ]
 
 if settings.DEBUG:
