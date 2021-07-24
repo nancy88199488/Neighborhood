8d62116d0283b0dcdee80b212ec243840fc22c9f
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-24 23:48:38 +0300

    added and modified the tests file

--- neighborhood/tests.py
+++ neighborhood/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        
+        self.new_profile = Profile(user_id=2,hood_id=3,bio="just testing", email='benardakaka484@gmail.com',name="Benard",profile_pic="default.jpeg")
+
+  
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0) 
+    
+    def tearDown(self):
+        Profile.objects.all().delete()
