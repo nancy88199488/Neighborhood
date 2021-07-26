b0c85bf2990d4eeb8d0eb805b9c78116104db36f
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-26 14:27:05 +0300

    updated the apps file

--- neighborhood/apps.py
+++ neighborhood/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NeighborhoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'neighborhood'
+
+    def read(self):
+        import neighborhood.signals
