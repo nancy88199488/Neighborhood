127b5107062936c18310583531287276ee686fd2
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-07-26 09:58:41 +0300

    modified the admin file

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Business,Neighborhood,Profile,Post
+from .models import Business,Neighborhood,Profile,Posts
 
 # Register your models here.
 
 admin.site.register(Business)
 admin.site.register(Neighborhood)
 admin.site.register(Profile)
-admin.site.register(Post)
+admin.site.register(Posts)
